17038cef15a77734738b7a8743581f30eca36836
JuanAlvarezGuerrero <alvarezguerrerojuan2003@gmail.com>
2024-06-05 21:07:08 -0300

    Magnet Upgrade

--- Arkanoid3D/Assets/Ball.cs
+++ Arkanoid3D/Assets/Ball.cs
@@ -11,15 +11,20 @@ public class Ball : MonoBehaviour
     public Vector3 velocity;
     public Vector3 direction; // Dirección de movimiento de la pelota
 
+    private bool magnetActive = false;
+    private bool catchedBall = false;
+    [SerializeField] Transform magnetPos;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        magnetPos = GameObject.Find("MagnetPos").GetComponent<Transform>();
     }
 
     public void UpdateBall()
     {
         _rb.velocity = direction * speed;
+        MagnetLogic();
         //transform.Translate(direction * speed * Time.deltaTime);                // Mover la pelota en su dirección a la velocidad especificada
     }
 
@@ -27,6 +32,13 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Player") && magnetActive)
+        {
+            Debug.Log("Catching the ball");
+            _rb.velocity = Vector3.zero;
+            speed = 0f;
+            catchedBall = true;
+        }
         // Cambiar la dirección de la pelota al colisionar con otro objeto
         direction = Vector3.Reflect(direction, collision.contacts[0].normal);
         direction.y = 0f; // Mantener la dirección en el eje Y en 0 para evitar movimientos verticales
@@ -49,5 +61,31 @@ public class Ball : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawRay(transform.position, direction * speed);
+    }
+
+    public void EnableMagnet()
+    {
+        magnetActive = true;
+    }
+    public void DisableMagnet()
+    {
+        magnetActive = false;
+    }
+    public bool isMagnetEnabled()
+    {
+        return magnetActive;
+    }
+    public void MagnetLogic()
+    {
+        if (catchedBall)
+        {
+            transform.position = magnetPos.position;
+        }
+        if (catchedBall && Input.GetKeyDown(KeyCode.Space))
+        {
+            SetNewDirection();
+            speed = 25;
+            catchedBall = false;
+        }
     }
 }
